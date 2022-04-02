96f1819cfda2a406a09ba981dd49e6f60b1197c0
nelstevens <nelson.stevens92@gmail.com>
2022-04-02 22:24:52 +0200

    split getApi

--- R/fct_getdata.R
+++ R/fct_getdata.R
@@ -1,14 +1,7 @@
 #' get api data
 #'
 #' @description get data from easyscore api
-#' @importFrom httr GET content stop_for_status
-#' @importFrom tibblify tibblify
-#' @importFrom tidyr unnest
-#' @importFrom dplyr filter
-#' @importFrom stringr str_detect
-#' @importFrom magrittr %>%
-#' @importFrom xml2 as_list
-#' @importFrom rlang .data
+#' @importFrom httr GET stop_for_status
 #'
 #'
 #' @noRd
@@ -20,8 +13,26 @@ getApi <- function(endpoint, query) {
     query = query
   )
   httr::stop_for_status(res)
+  make_df(res)
+}
+
+#' response to df
+#'
+#' @importFrom httr content
+#' @importFrom xml2 as_list
+#' @importFrom stringr str_detect
+#' @importFrom magrittr %>%
+#' @importFrom dplyr filter mutate_at
+#' @importFrom rlang .data
+#' @importFrom tidyr unnest
+#' @importFrom tibblify tibblify
+#' @noRd
+make_df <- function(res) {
   con <- httr::content(res) %>%
     as_list()
   df <- unnest(tibblify(con[[1]]))
-  df %>% filter(stringr::str_detect(.data$Player,"LIO"))
+  df2 <- df %>% filter(stringr::str_detect(.data$Player,"LIO"))
+  # generate column vector
+  cols <- names(df2)[!(names(df2) %in% c("Player", "Nationality", "dtCreated"))]
+  df2 %>% mutate_at(cols, as.numeric) %>% replace(is.na(.data), 0)
 }
