1fb9e1ed756080cf485549b7b2a400180e062226
nelstevens <nelson.stevens92@gmail.com>
2022-04-06 20:08:34 +0200

    bump testthat

--- tests/testthat/test-fct_getdata.R
+++ tests/testthat/test-fct_getdata.R
@@ -1,4 +1,5 @@
 test_that("getApi works", {
   expect_error(suppressWarnings(getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTea = "Individual"))))
+
   expect_error(suppressWarnings(getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))), NA)
 })
