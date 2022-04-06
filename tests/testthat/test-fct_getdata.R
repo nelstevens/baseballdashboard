87044e78eeed55db1c9804dd3457da79b455b36f
nelstevens <nelson.stevens92@gmail.com>
2022-04-06 21:26:05 +0200

    don't test for error

--- tests/testthat/test-fct_getdata.R
+++ tests/testthat/test-fct_getdata.R
@@ -1,6 +1,4 @@
 test_that("getApi works", {
-  expect_error(suppressWarnings(getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTea = "Individual"))))
-
   expect_s3_class(
     suppressWarnings(getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))),
     "data.frame"
