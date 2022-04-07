dccfb54ecde06b40b00580978a35e3cd822d2966
nelstevens <nelson.stevens92@gmail.com>
2022-04-07 22:57:27 +0200

    add module for offense

---
+++ R/mod_offense.R
@@ -0,0 +1,31 @@
+#' offense UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_offense_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' offense Server Functions
+#'
+#' @noRd 
+mod_offense_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+ 
+  })
+}
+    
+## To be copied in the UI
+# mod_offense_ui("offense_1")
+    
+## To be copied in the server
+# mod_offense_server("offense_1")
