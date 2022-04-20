e16f79080e3379035c90f9274b7f8334f9783e1e
Nelson Stevens <48679260+nelstevens@users.noreply.github.com>
2022-04-20 15:14:49 +0200

    check data

--- R/fct_getdata.R
+++ R/fct_getdata.R
@@ -37,11 +37,13 @@ make_df <- function(res, league) {
   con <- httr::content(res) %>%
     as_list()
   df <- unnest(tibblify(con[[1]]))
+  cat(str(df))
   if (league == 83) {
     df2 <- df %>% filter(stringr::str_detect(.data$Player,"LIO"))
   } else {
     df2 <- df %>% filter(stringr::str_detect(.data$Player,"WP4"))
   }
+  cat(str(df2))
   if (nrow(df2) == 0) stop(paste0("Dataframes is empty after subsetting", league))
   # generate column vector
   cols <- names(df2)[!(names(df2) %in% c("Player", "Nationality", "dtCreated"))]
