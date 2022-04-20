1b6fd3c3359cc7f6cde9186c4cdd9c20f644680d
nelstevens <nelson.stevens92@gmail.com>
2022-04-20 22:33:15 +0200

    introduce post

--- R/fct_getdata.R
+++ R/fct_getdata.R
@@ -19,6 +19,54 @@ getApi <- function(endpoint, query) {
   return(df)
 }
 
+#' get data via post
+#'
+#' @description get data from easyscore api via post method
+#' @importFrom httr POST stop_for_status
+#' @importFrom purrr map_dfr
+#'
+#' @noRd
+postApi <- function(endpoint, query) {
+  message(paste0("resquesting: ", endpoint))
+  baseurl <- "https://easyscore.com"
+  url <- paste0(baseurl, "/", endpoint)
+  res <- httr::POST(
+    url = url,
+    body = query,
+    encode = "json"
+  )
+  httr::stop_for_status(res)
+  make_df_post(res, query$League)
+}
+
+#' make df for post
+#'
+#' @importFrom httr content
+#' @importFrom stringr str_detect
+#' @importFrom dplyr filter mutate_at
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
+#' @noRd
+make_df_post <- function(res, league) {
+  . <- NULL
+  message(paste0("processing", league))
+  con <- httr::content(res)
+  df <- purrr::map_dfr(con, ~.x)
+  df <- df[,-c(1)]
+  if (league == 83) {
+    cat("subset for NLB")
+    df2 <- df %>% filter(stringr::str_detect(.data$Player,"LIO"))
+  } else {
+    cat("subset for 1. Liga")
+    df2 <- df %>% filter(stringr::str_detect(.data$Player,"WP4"))
+  }
+  cat(str(df2))
+  if (nrow(df2) == 0) stop(paste0("Dataframes is empty after subsetting", league))
+  # generate column vector
+  cols <- names(df2)[!(names(df2) %in% c("Player", "Nationality", "dtCreated"))]
+  df2 %>% mutate_at(cols, as.numeric) %>% replace(is.na(.), 0)
+}
+
 #' response to df
 #'
 #' @importFrom httr content
--- setup_data.R
+++ setup_data.R
@@ -1,39 +1,39 @@
 # laod all
 devtools::load_all()
-getApi <- purrr::insistently(getApi)
-getApi <- purrr::slowly(getApi)
+postApi <- purrr::insistently(postApi)
+postApi <- purrr::slowly(postApi)
 # get 2021 VR offense nlb
-VR21nlboff <- getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
+VR21nlboff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
 # get 2021 PL offense nlb
-PL21nlboff <- getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
+PL21nlboff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
 
 # get 2021 VR def nlb
-VR21nlbfld <- getApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
+VR21nlbfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
 # get 2021 PL offense nlb
-PL21nlbfld <- getApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
+PL21nlbfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
 
 # get 2021 VR pitching nlb
-VR21nlbpit <- getApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
+VR21nlbpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
 # get 2021 PL offense nlb
-PL21nlbpit <- getApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
+PL21nlbpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
 
 # get 2021 VR offense 1l
-VR21fstoff <- getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
+VR21fstoff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
 # get 2021 PL offense 1l
-#PL21fstoff <- getApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
+#PL21fstoff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
 
 # get 2021 VR def 1l
-VR21fstfld <- getApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
+VR21fstfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
 # get 2021 PL offense 1l
-#PL21fstfld <- getApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
+#PL21fstfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
 
 # get 2021 VR pitching 1l
-VR21fstpit <- getApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
+VR21fstpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
 # get 2021 PL offense 1l
-#PL21fstpit <- getApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
+#PL21fstpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
 
 # get list of object
-lst <- ls()[ls() != "getApi"]
+lst <- ls()[ls() != "postApi"]
 
 # write to fst
 purrr::walk(
