6ccddaf5e57f90c8bfcc134ff860f5cf5a682e17
nelstevens <nelson.stevens92@gmail.com>
2022-04-25 21:21:37 +0200

    get all years and rounds

--- setup_data.R
+++ setup_data.R
@@ -2,42 +2,53 @@
 devtools::load_all()
 postApi <- purrr::insistently(postApi)
 postApi <- purrr::slowly(postApi)
-# get 2021 VR offense nlb
-VR21nlboff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
-# get 2021 PL offense nlb
-PL21nlboff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
 
-# get 2021 VR def nlb
-VR21nlbfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
-# get 2021 PL offense nlb
-PL21nlbfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
+ends <- c("stats/Stats_Static.asmx/off", "stats/Stats_Static.asmx/fld", "stats/Stats_Static.asmx/pit")
+yrs <- 2016:2022
+cls <- list()
+for (i in yrs) {
+  for (j in ends) {
+    sub1 <- list(
+      list(
+        endpoint = j,
+        args = list(Year = i, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual")
+      )
+    )
+    cls <- c(cls, sub1)
+    sub2 <- list(
+      list(
+        endpoint = j,
+        args = list(Year = i, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual")
+      )
+    )
+    cls <- c(cls, sub2)
+    sub3 <- list(
+      list(
+        endpoint = j,
+        args = list(Year = i, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual")
+      )
+    )
+    cls <- c(cls, sub3)
+  }
 
-# get 2021 VR pitching nlb
-VR21nlbpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 83, Round = 53, Splits = "", IndOrTeam = "Individual"))
-# get 2021 PL offense nlb
-PL21nlbpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 83, Round = 303, Splits = "", IndOrTeam = "Individual"))
+}
 
-# get 2021 VR offense 1l
-VR21fstoff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
-# get 2021 PL offense 1l
-#PL21fstoff <- postApi("stats/Stats_Static.asmx/off", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
-
-# get 2021 VR def 1l
-VR21fstfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
-# get 2021 PL offense 1l
-#PL21fstfld <- postApi("stats/Stats_Static.asmx/fld", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
-
-# get 2021 VR pitching 1l
-VR21fstpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 84, Round = 57, Splits = "", IndOrTeam = "Individual"))
-# get 2021 PL offense 1l
-#PL21fstpit <- postApi("stats/Stats_Static.asmx/pit", list(Year = 2021, League = 84, Round = 410, Splits = "", IndOrTeam = "Individual"))
-
-# get list of object
-lst <- ls()[ls() != "postApi"]
-
-# write to fst
 purrr::walk(
-  lst,
-  function(x) fst::write_fst(base::get(x), paste0(app_sys("app/extdata"), "/", x, ".fst"))
+  cls,
+  function(x) {
+    rnd <- ifelse(x$args$Round == 303, "PL", "VR")
+    lge <- ifelse(x$args$League == 83, "nlb", "fst")
+    yr <- x$args$Year - 2000
+    typ <- stringr::str_split(x$endpoint, "/")[[1]][3]
+    nm <- paste0(rnd, yr, lge, typ)
+    path <- paste0(app_sys("app/extdata"), "/", nm, ".fst")
+    df <- tryCatch(
+      {
+        postApi(x$endpoint, x$args)
+      },
+      error = function(m) message(m)
+    )
+    if (!is.null(df)) fst::write_fst(df, path)
+  }
 )
 
